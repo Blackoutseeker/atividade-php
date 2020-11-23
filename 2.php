de4d5f5169fda3962aaab80faa3869f7db339b2d
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2020-11-23 18:25:38 -0300

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,12 @@
+<?php
+    $num = 17;
+    if($num % 10 == 0) {
+        echo 'É divisível por 10';
+    }
+    else if($num % 5 == 0) {
+        echo 'É divisível por 5';
+    }
+    else if($num % 2 == 0) {
+        echo 'É divisível por 2';
+    }
+?>
