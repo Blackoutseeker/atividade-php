3aaf701c00174cbe94f1dc602cdeed1fea2bb4d0
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2020-11-23 18:23:26 -0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,12 @@
+<?php
+    $num1 = 10;
+    $num2 = 5;
+    $soma = $num1 + $num2;
+    if($soma > 20) {
+        $soma += 8;
+    }
+    else {
+        $soma -= 5;
+    }
+    echo 'A soma é $soma';
+?>
