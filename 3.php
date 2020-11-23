fea851974fa6771a116741894dbc12086a5e8ed3
Blackoutseeker <56811005+Blackoutseeker@users.noreply.github.com>
2020-11-23 18:28:03 -0300

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = 'Eduarda';
+    $idade = 17;
+    $sexo = 'F';
+    if($sexo == 'F' && $idade < 25) {
+        echo 'ACEITA';
+    }
+    else {
+        echo 'NÃO ACEITA';
+    }
+?>
